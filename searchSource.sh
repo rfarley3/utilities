6d33300266acd96ec10fdb871b58ceb765da3ea3
rfarley <rfarley@mitre.org>
2014-12-31 09:32:55 -0500

    Make searchSource more robust

--- searchSource.sh
+++ searchSource.sh
@@ -18,25 +18,35 @@ if [ $# -eq 2 ] ; then
 	PATTERN="$2"
 fi
 
-INCL_PY="--include *.py"
-INCL_C="--include *.c --include *.h"
-INCL_CPP="--include *.cpp --include *.hpp"
-INCL_FL="--include *.flex"
-INCLUDES="${INCL_PY} ${INCL_C} ${INCL_CPP} ${INCL_FL}"
+#INCL_PY="--include *.py"
+#INCL_C="--include *.c --include *.h"
+#INCL_CPP="--include *.cpp --include *.hpp"
+#INCL_FL="--include *.flex"
+#INCL_CONF="--include *.conf --include *.yml"
+#INCLUDES="${INCL_PY} ${INCL_C} ${INCL_CPP} ${INCL_FL} ${INCL_CONF}"
+#EXCL_PY="--exclude *.pyc"
+#EXCL_C="--exclude *.o"
+#EXCL_EXE="--exclude *.out --exclude *.exe"
+#EXCLUDES="${EXCL_PY} ${EXCL_C} ${EXCL_EXE}"
 
 echo "----------------------------------------------------"
 echo "file|dir: ${FILE}"
 echo "pattern : ${PATTERN}"
-echo "includes: ${INCLUDES}"
-echo "----------------------------------------------------"
+#echo "includes: ${INCLUDES}"
+#echo "excludes: ${EXClUDES}"
 
 # search contents, for pattern, of files with names that match includes
-# -R              recursive
-# --color=always  output color escapes, regardless of the fact it detects a pipe
 # -n              makes it show line numbers
+# --color=always  output color escapes, regardless of the fact it detects a pipe
+# -s              don't show output about files it didn't process
+# -R              recursive
 # 2> /dev/null    discard all stderr
 # sed ...         make output cleaner by getting rid of root dir (note, rem if you want to see full paths)
-grep -n --color=always -R ${INCLUDES} "${PATTERN}" ${FILE} 2> /dev/null | sed -e "s|${FILE}/||" 
+CMD="grep -n --color=always --binary-files=without-match -s -R --exclude-dir .git ${EXCLUDES} ${INCLUDES} \"${PATTERN}\" ${FILE} 2> /dev/null"
+CMD="${CMD} | sed -e 's|${FILE}||' | sed -e 's|^/||'"
+echo "running : ${CMD}"
+echo "----------------------------------------------------"
+eval ${CMD}
 
 # TODO
 # work output st you show filename once
